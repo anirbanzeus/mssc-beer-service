4b0d4610c80f958455bb94ec385ea65e1e05b36c
anir <anirbanzeus@gmail.com>
2022-07-18 21:41:52 +0530

    Added Beer repository. Closes #6

---
+++ mssc-beer-service/src/main/java/learn/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package learn.springframework.msscbeerservice.repositories;
+
+import learn.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
